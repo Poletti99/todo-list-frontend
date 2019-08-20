fc3451bd5c430a6fe559a41ed7785bc8944f3354
Victor Poletti <victor.poletti@gastecnologia.com.br>
2019-08-20 14:10:18 -0300

    Criando os estilos globais da aplicaçao

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import history from './services/history';
+
+import { GlobalStyle } from './styles/global';
 
 function App() {
   return (
-    <div className="App">
-      Olar xuxu
-    </div>
+    <Router history={history}>
+      <GlobalStyle />
+    </Router>
   );
 }
 
